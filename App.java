2e43f6f412c2eee5ca465f0acb658722c10e3d32
TheXorcist <debayan.pog@gmail.com>
2024-03-12 20:15:19 +0530

    App.commit

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        TicTacToe ticTacToe = new TicTacToe();
+public class App{
+    public static void main(String[] args) throws Exception{
+        TicTacToe tictactoe = new TicTacToe();
+        
     }
 }
